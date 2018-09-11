669cfe3eb87be59e2d2f2da4e5083be365d241c4
Laura Kulas <laura.kulas@gmail.com>
2018-09-10 20:40:34 -0700

    updated port in config.js

--- test/test-auth.js
+++ test/test-auth.js
@@ -10,9 +10,6 @@ const { JWT_SECRET, TEST_DATABASE_URL } = require('../config');
 
 const expect = chai.expect;
 
-// This let's us make HTTP requests
-// in our tests.
-// see: https://github.com/chaijs/chai-http
 chai.use(chaiHttp);
 
 describe('Auth endpoints', function () {
