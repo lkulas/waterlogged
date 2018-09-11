d9d5c519c28e0a798647fcd7422de0e4fe34638a
Laura Kulas <laura.kulas@gmail.com>
2018-09-10 20:25:57 -0700

    updated config.js with mlab urls

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/jwt-auth-demo';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/jwt-auth-demo';
+exports.DATABASE_URL = process.env.MONGODB_URI || 'mongodb://heroku_4w445fn2:37edr83v718vp0vbuhcuf1fg1n@ds151612.mlab.com:51612/heroku_4w445fn2';
+exports.TEST_DATABASE_URL = process.env.MONGOLAB_BLACK_URI || 'mongodb://heroku_58v64c3h:895n4on9ok0nhb1ssruncrtmlq@ds151612.mlab.com:51612/heroku_58v64c3h';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
