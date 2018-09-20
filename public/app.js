3b2c84b6deee611b88eb34fbedcf1e848d0439b9
Laura Kulas <laura.kulas@gmail.com>
2018-09-20 10:53:06 -0700

    added delete plant function

--- public/app.js
+++ public/app.js
@@ -32,8 +32,8 @@ const MOCK_GARDEN_DATA = {
                     nextHarvest: function() {
                         return addDays(this.lastHarvested, this.harvestEvery)
                     },
-                    lastHarvested: new Date('2018-09-18'),
-                    lastWatered: new Date('2018-09-18')
+                    lastHarvested: new Date('2018-09-19'),
+                    lastWatered: new Date('2018-09-19')
                 },
                 {
                     name: "Carrots",
@@ -46,8 +46,8 @@ const MOCK_GARDEN_DATA = {
                     nextHarvest: function() {
                         return addDays(this.lastHarvested, this.harvestEvery)
                     },
-                    lastHarvested: new Date('2018-09-18'),
-                    lastWatered: new Date('2018-09-18')
+                    lastHarvested: new Date('2018-09-20'),
+                    lastWatered: new Date('2018-09-20')
                 },
             ],
         },
@@ -113,12 +113,18 @@ function getGarden(callbackFn) {
 };
 
 function displayGarden(data) {
-    for (let i = 0; i < data.gardens[0].plants.length; i++) {
-        $('#my-garden').append(
-        `<div>
-            <h3>${data.gardens[0].plants[i].name}</h3>
-            <button type="button" class="delete-button">Delete</button>
-        </div>`);
+    $('#plant-list').html('');
+    if (data.gardens[0].plants.length === 0) {
+        $('#plant-list').html(
+            `<p>There are no plants in your garden! Would you like to <span class="add-plant">add one</span>?</p>`
+            )
+    } else {
+        for (let i = 0; i < data.gardens[0].plants.length; i++) {
+            $('#plant-list').append(
+            `<div>
+                <h3>${data.gardens[0].plants[i].name}</h3>
+            </div>`);
+        }
     };
 };
 
@@ -127,21 +133,23 @@ function getAndDisplayGarden() {
 };
 
 function displayPlantDetails(data, target) {
-    const indexOf = data.gardens[0].plants.findIndex(i => i.name === `${target}`);
+    const indexOf = data.gardens[0].plants.findIndex(i => i.name === target);
     console.log(indexOf);
     const plant = data.gardens[0].plants[indexOf];
     $('#plant-details').html(
-        `<h2>${plant.name}</h2>
-        <ul>
-            <li>Planted: ${plant.planted.toString()}</li>
-            <li>Water every: ${plant.waterEvery} days</li>
-            <li>Water on: ${plant.nextWater()}</li>
-            <li>Harvest every: ${plant.harvestEvery} days</li>
-            <li>Harvest on: ${plant.nextHarvest()}</li>
-        </ul>
-        <button type="button" class="edit-button">Edit</button>
-        <button type="button" class="delete-button">Delete</button>
-        <button type="button" class="back-button">Back</button>`);
+        `<div class="${plant.name}">
+            <h2 class="plant-name">${plant.name}</h2>
+            <ul>
+                <li>Planted: ${plant.planted.toString()}</li>
+                <li>Water every: ${plant.waterEvery} days</li>
+                <li>Water on: ${plant.nextWater()}</li>
+                <li>Harvest every: ${plant.harvestEvery} days</li>
+                <li>Harvest on: ${plant.nextHarvest()}</li>
+            </ul>
+            <button type="button" class="edit-button">Edit</button>
+            <button type="button" class="delete-button">Delete</button>
+            <button type="button" class="back-button">Back</button>
+        </div>`);
 };
 
 function watchPlantDetailsClick() {
@@ -181,10 +189,24 @@ function watchLogout() {
     });
 };
 
+function watchDeletePlant() {
+    $('#plant-details').on('click', '.delete-button', event => {
+        const array = MOCK_GARDEN_DATA.gardens[0].plants;
+        const target = event.target.closest('div').className;
+        const indexOf = array.findIndex(i => i.name === target);
+        console.log(indexOf);
+        array.splice(indexOf, 1);
+        $('#plant-details').prop('hidden', true);
+        getAndDisplayGarden();
+        $('#my-garden').prop('hidden', false);
+    })
+}
+
 $(function() {
     getAndDisplayGarden();
     watchPlantDetailsClick();
     watchGoBack();
     watchLoginSubmit();
     watchLogout();
+    watchDeletePlant();
 });
