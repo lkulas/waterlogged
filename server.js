5b6850a857b0f6ceb47f52c3d3b4b7b4b06c9c60
Laura Kulas <laura.kulas@gmail.com>
2018-09-23 17:38:18 -0700

    initial working GET endpoint

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ passport.use(jwtStrategy);
 
 app.use('/api/users/', usersRouter);
 app.use('/api/auth/', authRouter);
-app.use('/api/garden/', gardenRouter);
+app.use('/api/my-garden/', gardenRouter);
 app.use(express.static('public'));
 
 const jwtAuth = passport.authenticate('jwt', { session: false });
